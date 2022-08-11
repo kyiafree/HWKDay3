a7fa7576b0ed320903e6544edd799fbf6604a6db
kyiafree <110514670+kyiafree@users.noreply.github.com>
2022-08-11 08:54:42 -0400

    Add files via upload

---
+++ example.js
@@ -0,0 +1,53 @@
+
+
+// for(let i=2; i<11; i+=2){
+//     console.log(i)
+// }
+
+// for(i=1; i <=9 ; i++){
+//     if(i % 2 === 0)
+//     console.log(i)
+// }
+
+
+
+//  let grade =13
+ 
+// if(grade >=80) {
+//     console.log("You did a good job")
+// }else if(grade >= 70) {
+//     console.log("Keep trying")
+// }else if(grade >= 60) {
+//     console.log("ehhhh")
+// }else if(grade >= 55) {
+//     console.log("Not too good")
+// } else {
+//     console.log("Bad Grade But You'll Get Em Next Time")
+// }
+
+
+
+
+// for(let i= 1; i<=100; i++){
+//     if(i % 3 === 0)
+//     console.log(i)
+// }
+
+// for(let i=1; i<=100; i++){
+//     if(i % 3 ===0 && i % 5 === 0){
+//         console.log("FizzBuzz")
+//     }else { console.log(i)
+
+//     }
+    
+// }
+
+for ( let i = 0; i < 5; i++){
+    console.log('#')
+    console.log('##');
+    console.log('###');
+    console.log('####')
+}
+
+
+
